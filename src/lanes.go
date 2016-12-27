0574469ce5034a1abc48dc2d0ef6a6364652c0a1
David Welch <david@davidwelch.co>
2016-12-26 23:21:33 -0700

    yaml configuration parsing almost in place

--- src/lanes.go
+++ src/lanes.go
@@ -2,14 +2,64 @@ package main
 
 
 import (
-	"os"
-	"github.com/urfave/cli"
-	"fmt"
-	"github.com/aws/aws-sdk-go/aws/session"
 	ec2util "./ec2"
-	"ssh"
+	"./ssh"
+
 	"log"
+	"fmt"
+	"os"
+	"os/user"
+	"github.com/urfave/cli"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/davecgh/go-spew/spew"
+	"gopkg.in/yaml.v2"
+	"strings"
+	"io/ioutil"
+	"errors"
 )
+
+
+func readYaml(path string)(map[interface{}]interface{}, error ) {
+	bytes, err := ioutil.ReadFile(path)
+	if( err != nil ){
+		return nil, err
+	}
+
+	val := make(map[interface{}]interface{} )
+	err = yaml.Unmarshal(bytes, &val)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("--- t:\n%v\n\n", val)
+
+	return val, nil
+}
+
+// recursively iterates down a "dot-path" of a map – returning the matching element
+func maps(m map[interface{}]interface{}, key string) (interface{}, error){
+	var ok bool
+
+	keys := strings.Split(key, ".")
+
+	log.Printf("Looking at %v", keys)
+
+	for i, key := range keys {
+		log.Printf("Looking at %v", key)
+
+		val := m[key]
+
+		if i == len(keys) - 1 {
+			return val, nil
+		} else if m, ok = val.(map[interface{}]interface{}); !ok {
+			log.Printf("Non-Map at %v of type %v", key, val)
+			return nil, errors.New("Received a non-map value along the path at " + key)
+		}else {
+			m = val.(map[interface{}]interface{})
+		}
+	}
+
+	return nil, errors.New("Should have never gotten here!")
+}
 
 
 func main() {
@@ -21,8 +71,42 @@ func main() {
 		listInstances(),
 	}
 
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal( "Failed to determine current user: ", err )
+	}
 
-	app.Run(os.Args)
+	base := user.HomeDir + "/.lanes/"
+	config, err := readYaml( base + "lanes.yml")
+	if( err != nil ){
+		log.Fatal("Failed to load base lanes configuration file: ", err)
+	}
+
+	var profileStr string
+	var ok bool
+
+	profile := config["profile"]
+	if profileStr, ok = profile.(string); !ok {
+		log.Fatal("Problem getting profile ")
+	}
+	if(  len(strings.TrimSpace(profileStr)) < 1 ){
+		log.Fatal("Failed to load base lanes configuration's profile: ", err)
+	}
+
+	log.Printf("Connecting " + base + profileStr + ".yml")
+	config, err = readYaml( base + profileStr + ".yml")
+	if( err != nil ){
+		log.Fatal("Failed to load base lanes configuration file: ", err)
+	}
+
+	spew.Dump(config)
+
+	raw, e := maps(config, "ssh.mods.dev")
+	log.Printf("path %v %v \n", e, raw)
+
+
+	//app.Run(os.Args)
+	log.Printf("test %v \n", os.Args)
 
 }
 
